cf05d5e19e96216cbd4896f413f9ac91f7d81180
harshinitheboss <99989631+harshinitheboss@users.noreply.github.com>
2026-03-02 11:38:59 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,54 @@
-/**
- * OOPSBannerApp
- *
- * This application prints the literal text "OOPS" to the console.
- *
- * @author Nithishkumar
- * @version 1.0
- */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(String.join(" ",
+                "  ***  ",
+                "  ***  ",
+                " ****** ",
+                " ***** "
+        ));
+
+        System.out.println(String.join(" ",
+                " *   * ",
+                " *   * ",
+                " *    *",
+                " *    *"
+        ));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*     "
+        ));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*      ",
+                " **** "
+        ));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "     *"
+        ));
+
+        System.out.println(String.join(" ",
+                " *   * ",
+                " *   * ",
+                "*      ",
+                "*     *"
+        ));
+
+        System.out.println(String.join(" ",
+                "  ***  ",
+                "  ***  ",
+                "*      ",
+                " ***** "
+        ));
     }
 }
